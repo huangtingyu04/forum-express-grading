4077c27b5cbea7a76bb5be8472664b1dee56d442
Emery <apple@AppledeMacBook-Air.local>
2023-05-18 22:35:08 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-// 新增，載入 controller
-const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurant)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
